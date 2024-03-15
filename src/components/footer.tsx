62698d2fd58d0709d67024db143d19b207674854
Marcel Léon Mrgic <mrgic@campus.tu-berlin.de>
2024-03-15 19:12:07 +0100

    fixed Footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,7 +5,7 @@ export default function Footer(){
         <div className="bg-gradient-to-r from-indigo-700 via-indigo-500 to-indigo-600 w-screen h-10 absolute bottom-0 text-white items-center flex">
 
             <Link className="pl-5" href={"#"}>Impressum</Link>
-            <Link className="pl-5" href={"#"}>AGB's</Link>
+            <Link className="pl-5" href={"#"}>AGB&apos;s</Link>
             <Link className="pl-5" href={"#"}>Support</Link>
 
 
