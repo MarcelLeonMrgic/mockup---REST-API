4317c5cf6d3c393550c30f53fedbbdfb8618cefd
Marcel Léon Mrgic <mrgic@campus.tu-berlin.de>
2024-03-20 13:10:11 +0100

    Antrag create fix

--- src/app/api/antraege/route.ts
+++ src/app/api/antraege/route.ts
@@ -3,14 +3,14 @@ import Antrag from "@/models/antrag";
 import { NextResponse } from "next/server";
 
 export async function POST(request: any) {
-    const { titel, beschreibung } = await request.json();
+    const { titel, aUE , zweckbestimmung , kostenNutzenSchaetzung, verzeichnisHardSoftware ,konzeptZugriffsberechtigung, schnittstellenITSysteme, organisatorischeUndZeitlicheProjektabwicklung, alternativen, listePersBezDaten , zweckPersBezDaten, artDatenerfassung, sicherungVerwaltungLoeschungPersBezDaten, genehmigt} = await request.json();
     const isConnected = await connectMongoDB();
 
     if (!isConnected) {
         return NextResponse.json({ message: "Fehler beim Verbinden mit MongoDB" }, { status: 500 });
     }
 
-    await Antrag.create({ titel, beschreibung });
+    await Antrag.create({ titel, aUE ,zweckbestimmung , kostenNutzenSchaetzung,verzeichnisHardSoftware, konzeptZugriffsberechtigung, schnittstellenITSysteme, organisatorischeUndZeitlicheProjektabwicklung, alternativen, listePersBezDaten, zweckPersBezDaten, artDatenerfassung, sicherungVerwaltungLoeschungPersBezDaten,genehmigt});
     return NextResponse.json({ message: "Antrag erstellt" }, { status: 201 });
 }
 
