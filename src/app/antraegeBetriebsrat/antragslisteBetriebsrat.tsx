b5329e637de630ca554b92d4b17b95c4ac6de32e
Marcel Léon Mrgic <mrgic@campus.tu-berlin.de>
2024-03-20 14:31:33 +0100

    Antragsliste des Betriebsrat

--- src/app/antraegeBetriebsrat/antragslisteBetriebsrat.tsx
+++
@@ -1,5 +0,0 @@
-export default function AntragsListeBetriebsrat(){
-return(<div>
-    <h1>hi betrantr</h1>
-</div>)
-}
---
+++ src/app/antraegeBetriebsrat/page.tsx
@@ -0,0 +1,5 @@
+export default function AntraegeBetriebsrat(){
+return(<div>
+    <h1>hi betrantr</h1>
+</div>)
+}
