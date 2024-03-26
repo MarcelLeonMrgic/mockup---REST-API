2280fab01fbf4ba7de5140d22023b1b6183c8a43
Marcel Léon Mrgic <mrgic@campus.tu-berlin.de>
2024-03-26 16:05:06 +0100

    hi

--- src/app/(dashboard)/arbeitgeber/page.tsx
+++ src/app/(dashboard)/arbeitgeber/page.tsx
@@ -5,7 +5,6 @@ import NewsletterHeader from "@/components/newletter";
 export default function Arbeitgeber(){
     return(
         <>
-            <h1>veränderung</h1>
         <MenuArbeitgeber/>
             <NewsletterHeader/>
 
